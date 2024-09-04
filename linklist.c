c1530a24b74505e04c2e5d8deda53a5174cdd1d1
ananyap2024 <raoananya2020@gmail.com>
2024-09-04 20:23:20 +0530

    Add files via upload

--- linklist.c
+++ linklist.c
@@ -7,10 +7,36 @@ struct node
 	} *head, *temp, *newnode;
 
 int count, pos, i=1;
+
+void create()
+{
+	int ch=1;	
+	head=0;
+	
+	while(ch==1)
+	{
+	newnode = (struct node*)malloc(sizeof(struct node));
+	printf("enter data for creation:");
+	scanf("%d", &newnode->data);
+	newnode->next=0;
+	
+	if(head==0)
+	{
+		head=temp=newnode;
+	}
+	else
+	{
+		temp->next=newnode;
+		temp=newnode;
+	}
+	printf("do you want to continue(0,1)?");
+	scanf("%d", &ch);
+	}
+}
 		
 void insbeg()
 {	newnode = (struct node*)malloc(sizeof(struct node));
-	printf("enter data for insertion:");
+	printf("enter data for insertion at beginnig:");
 	scanf("%d", &newnode->data);
 	newnode->next = head;
 	head = newnode;
@@ -19,7 +45,7 @@ void insbeg()
 void insend()
 {
 	newnode = (struct node*)malloc(sizeof(struct node));
-	printf("enter data for insertion:");
+	printf("enter data for insertion at end:");
 	scanf("%d", &newnode->data);
 	temp=head;
 	while(temp->next!=0)
@@ -35,18 +61,34 @@ void insend()
 void inspos()
 {
 	temp=head;
-	printf("enter position:");
+	printf("enter position for insertion:");
 	scanf("%d", &pos);
+	if(pos==1)
+	{
+		insbeg();
+	}
+	else if(pos==count)
+	{
+		insend();
+	}
+	else if(pos<i || pos>count)
+	{
+		printf("Invalid position, no insertion\n");
+	}
+	else
+	{
+		
 		while(i<pos)
 		{
 			temp=temp->next;
 			i++;
 		}
-	newnode = (struct node*)malloc(sizeof(struct node));
-	printf("enter data for insertion:");
-	scanf("%d", &newnode->data);
-	newnode->next = temp->next;
-	temp->next = newnode;
+		newnode = (struct node*)malloc(sizeof(struct node));
+		printf("enter data for insertion at position:");
+		scanf("%d", &newnode->data);
+		newnode->next = temp->next;
+		temp->next = newnode;
+	}
 			
 }
 
@@ -86,6 +128,16 @@ void delpos()
 	temp=head;
 	printf("enter position for deletion:");
 	scanf("%d", &pos);
+	if(pos==1)
+	{
+		delbeg();
+	}
+	else if(pos==count)
+	{
+		delend();
+	}
+	else
+	{
 	while(i<pos-1)
 	{
 		temp=temp->next;
@@ -94,73 +146,54 @@ void delpos()
 	nextnode=temp->next;
 	temp->next=nextnode->next;
 	free(nextnode);
+	}
 }
 
-int main()
+void main()
 {
-	void insbeg();
-	void insend();
-	void inspos();
-	void delbeg();
-	void delend();
-	void delpos();
-	int ch=1;	
-	head=0;
-	
-	while(ch==1)
-	{
-	newnode = (struct node*)malloc(sizeof(struct node));
-	printf("enter data:");
-	scanf("%d", &newnode->data);
-	newnode->next=0;
-	
-	if(head==0)
-	{
-		head=temp=newnode;
-	}
-	else
-	{
-		temp->next=newnode;
-		temp=newnode;
-	}
-	printf("do you want to continue(0,1)?");
-	scanf("%d", &ch);
-	}
-
 	int choice;
-	printf("\nchoice:\n1- insbeg\n2- insend\n3- inspos\n4- delbeg\n5- delend\n6- delpos\nenter choice (1, 2, 3, 4, 5, 6):");
-	scanf("%d", &choice);
-	switch(choice)
+	do
 	{
-		case '1':
-			insbeg();
-			break;
-			
-		case '2':
-			insend();
-			break;
-			
-		case '3':
-			inspos();
-			break;
-			
-		case '4':
-			delbeg();
-			break;
-			
-		case '5':
-			delend();
-			break;
-			
-		case '6':
-			delpos();
-			break;
-			
-		/*default:
-			printf("invalid choice:");*/		
-	}
+		printf("\nenter choice:\n1- create\n2- insbeg\n3- insend\n4- inspos\n5- delbeg\n6- delend\n7- delpos");
+		printf("\n(1, 2, 3, 4, 5, 6, 7):");
+		scanf("%d", &choice);
+		switch(choice)
+		{
+			case '1':
+				create();
+				break;
+				
+			case '2':
+				insbeg();
+				break;
+				
+			case '3':
+				insend();
+				break;
+				
+			case '4':
+				inspos();
+				break;
+				
+			case '5':
+				delbeg();
+				break;
+				
+			case '6':
+				delend();
+				break;
+				
+			case '7':
+				delpos();
+				break;
+				
+			//default:
+			//	printf("invalid choice!");		
+		}
+	}while(choice!=0);
 	
-
+	
+	//display
 	temp=head;
 	while(temp!=0)
 	{	
@@ -168,6 +201,16 @@ int main()
 		printf("%d\t", temp->data);
 		temp = temp->next;
 	}
-	printf("\nlength is %d", count);
+	printf("\nlength is %d", count);	
 
+	//function defination
+	void create();
+	void insbeg();
+	void insend();
+	void inspos();
+	void delbeg();
+	void delend();
+	void delpos();
+	
+	
 }
